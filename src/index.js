5fdd5be205d29e5fcac940d84907be4e71fa9802
Nicholas Crow <dreamer.lviv@gmail.com>
2017-02-24 21:44:14 +0200

    add 2 separated modules/pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // import _ from 'lodash';
 var style = require('./style/globalStyle.css');
-var messages = require('./messages');
+// var messages = require('./messages');
 
 // import Button from './button';
 // import User from './image';
@@ -14,23 +14,46 @@ var messages = require('./messages');
 //  </p>
 // `);
 
-import { multiply } from './mathStuff';
+// import { multiply } from './mathStuff';
 
 // const newMessage = () => (multiply(3,3));
-const newMessage = () => (`
-    <div class="${style.box}">
-        DEV: ${DEVELOPMENT.toString()}<br>
-        PROD: ${PRODUCTION.toString()}<br>
-    </div>
-`);
+// const newMessage = () => (`
+//     <div class="${style.box}">
+//         DEV: ${DEVELOPMENT.toString()}<br>
+//         PROD: ${PRODUCTION.toString()}<br>
+//     </div>
+// `);
 
 // var newMessage = () => (Button.button);
 
 var app = document.getElementById('app');
 // app.innerHTML = '<p>' + messages.hi + ' ' + messages.event + '</p>';
-app.innerHTML = newMessage();
-
+// app.innerHTML = newMessage();
 // Button.attachEl();
+
+app.innerHTML = `
+    <div id="menu">
+        <button id="loadPage1">Load Page 1</button>
+        <button id="loadPage2">Load Page 2</button>
+        <div id="content">
+            <h1>Home</h1>
+        </div>
+    </div>
+`;
+
+document.getElementById('loadPage1').addEventListener('click', () => {
+    System.import('./page1')
+        .then(pageModule => {
+            document.getElementById('content').innerHTML = pageModule.default;
+        });
+});
+document.getElementById('loadPage2').addEventListener('click', () => {
+    System.import('./page2')
+        .then(pageModule => {
+            document.getElementById('content').innerHTML = pageModule.default;
+        });
+});
+
 
 if (DEVELOPMENT) {
     if (module.hot) {
